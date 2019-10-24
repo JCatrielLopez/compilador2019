1207a4d27311ac9c32e9fd0275990770ee14abe4
jcatrielopez <jcatrielopez@gmail.com>
2019-10-24 15:54:25 -0300

    generados Terceto y AdminTercetos

---
+++ src/main/java/AdminTercetos.java
@@ -0,0 +1,40 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class AdminTercetos {
+
+    public static LinkedHashMap<String, Terceto> tercetos = new LinkedHashMap<>();
+    public static Stack<String> pila = new Stack<>();
+
+
+    public static Terceto getTerceto(String id) {
+        return tercetos.get(id);
+    }
+
+    public static void addTerceto(Terceto t) {
+        tercetos.put(t.getId(), t);
+    }
+
+    public static void push(String id) {
+        pila.push(id);
+    }
+
+    public static String pop() {
+        if (!pila.isEmpty())
+            return pila.pop();
+        return null;
+    }
+
+    public static int cantTercetos() {
+        return tercetos.size();
+    }
+
+    public String toString() {
+        StringBuilder out = new StringBuilder();
+        for (Map.Entry<String, Terceto> entry : tercetos.entrySet())
+            out.append(entry.getValue() + "\n"); // entry: (k, V)
+
+        return out.toString();
+    }
+}
---
+++ src/main/java/Terceto.java
@@ -0,0 +1,67 @@
+
+public class Terceto {
+
+    static int cont = 0;
+    String id;
+    String operacion;
+    String operando1;
+    String operando2;
+
+    public Terceto(String operacion, String operando1, String operando2) {
+        cont++;
+        this.id = "[" + cont + "]";
+        this.operacion = operacion;
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+    }
+
+    public Terceto(String operacion) {
+        cont++;
+        this.id = "[" + cont + "]";
+        this.operacion = operacion;
+        this.operando1 = null;
+        this.operando2 = null;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setOperador(int pos, String operador) {
+        if (pos == 1)
+            if (this.operando1 == null) //Para no pisar tercetos ya definidos. Sacarlo si hace falta en el futuro.
+                this.operando1 = operador;
+            else if (this.operando2 == null)
+                this.operando2 = operador;
+    }
+
+    public String getOperando1() {
+        return operando1;
+    }
+
+    public String getOperando2() {
+        return operando2;
+    }
+
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public String toString() {
+        StringBuilder out = new StringBuilder();
+
+        out.append(this.id)
+                .append(": ")
+                .append("(")
+                .append(this.operacion)
+                .append(", ")
+                .append(this.operando1)
+                .append(", ")
+                .append(this.operando2)
+                .append(")");
+
+        return out.toString();
+    }
+
+
+}
