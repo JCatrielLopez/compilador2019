5a9e1e196d87c48aad7e17ca7767d16ea84faa79
jcatrielopez <jcatrielopez@gmail.com>
2019-10-31 18:32:32 -0300

    Cree clase Error

---
+++ src/main/java/globals/Error.java
@@ -0,0 +1,22 @@
+package globals;
+
+import java.util.ArrayList;
+
+public class Error {
+
+    private static ArrayList<String> errores = new ArrayList<>();
+
+    public static void add(String msg) {
+        errores.add(msg);
+    }
+
+    public static void print() {
+        for (String msg : errores) {
+            Printer.print(msg, Color.RED);
+        }
+    }
+
+    public static void remove(String msg) {
+        errores.remove(msg);
+    }
+}
