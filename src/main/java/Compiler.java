a47fe587ea67bce675f71a335f54129464f0f5c6
jcatrielopez <jcatrielopez@gmail.com>
2019-11-21 11:31:26 -0300

    Arregle los parametros del .jar

--- src/main/java/Compiler.java
+++ src/main/java/Compiler.java
@@ -1,14 +1,17 @@
 import assembler.AdminTercetos;
 import assembler.AssemblerGen;
 import globals.Error;
-import globals.Printer;
 import globals.SymbolTable;
 import lexer.Lexer;
+import org.apache.commons.cli.*;
 import org.apache.commons.io.FilenameUtils;
 import org.fusesource.jansi.AnsiConsole;
 import parser.Parser;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 class Compiler {
 
@@ -16,69 +19,50 @@ class Compiler {
     AnsiConsole.systemInstall();
     SymbolTable.init();
 
-    String source_path = "";
-    boolean al_verbose = false;
-    boolean as_verbose = false;
-    boolean gc_verbose = false;
-    boolean ts_verbose = false;
-    boolean tercetos_verbose = false;
+    Options options = new Options();
+    options.addOption("i", true, "Direccion del archivo a compilar.");
+    options.addOption("al", false, "Imprime informacion del analisis lexico.");
+    options.addOption("as", false, "Imprime informacion del analisis sintactico.");
+    options.addOption("ts", false, "Imprime informacion de la tabla de simbolos.");
+    options.addOption("t", false, "Imprime informacion de la generacion de tercetos.");
+    options.addOption("c", false, "Genera codigo assembly y traduce a codigo maquina.");
+    options.addOption("h", false, "Imprime informacion de ayuda.");
 
-    for (int i = 0; i < args.length; i++) {
-      switch (args[i]) {
-        case "-i":
-          if (i + 1 < args.length) {
-            source_path = args[i + 1];
-            File tempFile = new File(source_path);
-            if (!tempFile.exists()) {
-              Printer.print("El archivo ingresado no existe.");
-              return;
-            }
-          } else {
-            Printer.print("No se ingreso ningun archivo.");
-            return;
-          }
-          break;
-        case "-al":
-          al_verbose = true;
-          break;
-        case "-as":
-          as_verbose = true;
-          break;
-        case "-g":
-          gc_verbose = true;
-          break;
-        case "-ts":
-          ts_verbose = true;
-          break;
-        case "-t":
-          tercetos_verbose = true;
-          break;
-        case "-h":
-          if (!(al_verbose || as_verbose || gc_verbose || ts_verbose || tercetos_verbose))
-            print_help();
-          return;
-        default:
-          // Ignoramos los argumentos que no son validos.
-      }
+    CommandLineParser cmdparser = new DefaultParser();
+    CommandLine cmd = cmdparser.parse(options, args);
+
+    if (cmd.hasOption("h")) {
+      new HelpFormatter().printHelp("compiler", options);
+      return;
     }
 
-    FileInputStream source_file = new FileInputStream(source_path);
+    FileInputStream source_file;
+    String source_path;
+    if (cmd.hasOption("i")) {
+      source_path = cmd.getOptionValue("i");
+      File tempFile = new File(source_path);
+      if (!tempFile.exists())
+        throw new IOException("No existe el archivo " + source_path);
+      else
+        source_file = new FileInputStream(source_path);
+    } else
+      throw new ParseException("Falta el argumento -i.");
 
     StringBuilder sb = new StringBuilder();
 
     while (source_file.available() != 0) sb.append((char) source_file.read());
     source_file.close();
 
-    if (al_verbose || as_verbose) {
+    if (cmd.hasOption("al") || cmd.hasOption("as")) {
       System.out.println(
               String.format(
                       "%5s %s %3s %s %s %-50s %s %s", "Linea ", "|", "Desde", "|", " ", "Info", " ", "|"));
       System.out.println("-----------------------------------------------------------------------");
     }
 
-    Lexer al = new Lexer(sb, al_verbose);
+    Lexer al = new Lexer(sb, cmd.hasOption("al"));
 
-    Parser parser = new Parser(al, as_verbose);
+    Parser parser = new Parser(al, cmd.hasOption("as"));
     parser.run();
 
     if (!Error.isEmpty()) {
@@ -91,7 +75,7 @@ class Compiler {
       System.out.println("-----------------------------------------------------------------------");
       Error.print();
     } else {
-      if (tercetos_verbose) {
+      if (cmd.hasOption("t")) {
         System.out.println(
                 "-----------------------------------------------------------------------");
         System.out.println(
@@ -101,36 +85,29 @@ class Compiler {
         System.out.println(AdminTercetos.print());
       }
 
-      String source_name = "tests/asd";
-      String tercetos_path = source_name + ".t";
-      String assembler_path = source_name + ".asm";
-      String obj_path = source_name + ".obj";
+      if (cmd.hasOption("c")) {
+        String source_name = FilenameUtils.removeExtension(source_path);
+        String tercetos_path = source_name + ".t";
+        String assembler_path = source_name + ".asm";
+        String obj_path = source_name + ".obj";
 
-      // Guardo los tercetos en archivo.t
-      FileOutputStream tercetos_file = new FileOutputStream(tercetos_path);
+        // Guardo los tercetos en archivo.t
+        FileOutputStream tercetos_file = new FileOutputStream(tercetos_path);
 
-      tercetos_file.write(AdminTercetos.print().getBytes());
+        tercetos_file.write(AdminTercetos.print().getBytes());
 
-      // Genero las instrucciones assembly
-      AssemblerGen.translate(assembler_path);
+        // Genero las instrucciones assembly
+        AssemblerGen.translate(assembler_path);
 
-      // Genero el archivo.exe
-      Process p = Runtime.getRuntime().exec("\\masm32\\bin\\ml /c /Zd /coff " + assembler_path);
-      BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-      BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+        // Genero el archivo exe
+        Process p;
 
-      System.out.println(stdInput.toString());
-      System.out.println(stdError.toString());
-
-      p = Runtime.getRuntime().exec("\\masm32\\bin\\Link /SUBSYSTEM:CONSOLE" + obj_path);
-      stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-      stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-
-      System.out.println(stdInput.toString());
-      System.out.println(stdError.toString());
+        p = Runtime.getRuntime().exec("\\masm32\\bin\\ml /c /Zd /coff " + assembler_path);
+        p = Runtime.getRuntime().exec("\\masm32\\bin\\Link /SUBSYSTEM:CONSOLE" + obj_path);
+      }
     }
 
-    if (ts_verbose) SymbolTable.print();
+    if (cmd.hasOption("ts")) SymbolTable.print();
 
     AnsiConsole.systemUninstall();
   }
